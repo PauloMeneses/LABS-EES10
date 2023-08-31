a3995795927c603d5f9fc078efbecb86bcd5ec8f
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2023-08-31 19:52:35 -0300

    fix: fixed value for applied step and variable names

--- lab2/lab3_calculos_20.m
+++ lab2/lab3_calculos_20.m
@@ -1,3 +1,4 @@
+clear all;
 load("20_percent.mat")
 plot(out.simout_30.Time, out.simout_30.Data(1:end,2));
 
@@ -8,7 +9,7 @@ yss20 = 23.355 - 19.575;
 Mp20 = (yp20-yss20)/yss20;
 
 ksi20 = -log(Mp20)/sqrt(pi^2 + (log(Mp20))^2);
-omega_n20 = pi/(tp20*sqrt(1-ksi^2));
+omega_n20 = pi/(tp20*sqrt(1-ksi20^2));
 k20 = deg2rad(yss20)/(15-13.03); %amplitude do degrau de entrada eh (15-13.03)
 
 alfa20  = omega_n20^2/cos(deg2rad(20))
--- lab2/lab3_calculos_40.m
+++ lab2/lab3_calculos_40.m
@@ -1,3 +1,5 @@
+clear all;
+
 load("40_percent.mat")
 plot(out.simout_30.Time, out.simout_30.Data(1:end,2));
 
@@ -8,8 +10,8 @@ yss40 = 48.69 - 43.155;
 Mp40 = (yp40-yss40)/yss40;
 
 ksi40 = -log(Mp40)/sqrt(pi^2 + (log(Mp40))^2);
-omega_n40 = pi/(tp40*sqrt(1-ksi^2));
-k40 = deg2rad(yss40)/(preencher_aqui); %amplitude do degrau a ser preenchida.
+omega_n40 = pi/(tp40*sqrt(1-ksi40^2));
+k40 = deg2rad(yss40)/(30-25.16); %amplitude do degrau a ser preenchida.
 % 
 % coloca o final menos o que estava antes, verificar o arquivo _20
 %
