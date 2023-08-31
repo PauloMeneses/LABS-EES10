dc13a877c0c9aa94de0092bfea874af84b36e0f4
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2023-08-31 13:16:26 -0300

    feat: add instructions to calculate gamma

--- lab2/lab3_calculos_40.m
+++ lab2/lab3_calculos_40.m
@@ -9,7 +9,10 @@ Mp40 = (yp40-yss40)/yss40;
 
 ksi40 = -log(Mp40)/sqrt(pi^2 + (log(Mp40))^2);
 omega_n40 = pi/(tp40*sqrt(1-ksi^2));
-k40 = deg2rad(yss40)/(preencher_aqui); %amplitude do degrau de entrada eh 
+k40 = deg2rad(yss40)/(preencher_aqui); %amplitude do degrau a ser preenchida.
+% 
+% coloca o final menos o que estava antes, verificar o arquivo _20
+%
 
 alfa40  = omega_n40^2/cos(deg2rad(40))
 beta40  = 2*ksi40*omega_n40
