36d21111f51fcdc4046f9c9ceb347e0e002b8ada
Paulo Vitor M.Andrade <100442352+itapops@users.noreply.github.com>
2023-08-31 20:50:22 -0300

    Resolva conflitos e faça as alterações necessárias

---
+++ lab2/lab3_calculos_20.m
@@ -0,0 +1,17 @@
+clear all;
+load("20_percent.mat")
+plot(out.simout_30.Time, out.simout_30.Data(1:end,2));
+
+tp20 = 20.793 - 20.024;
+yp20 = 24.705 - 19.575;
+yss20 = 23.355 - 19.575;
+
+Mp20 = (yp20-yss20)/yss20;
+
+ksi20 = -log(Mp20)/sqrt(pi^2 + (log(Mp20))^2);
+omega_n20 = pi/(tp20*sqrt(1-ksi20^2));
+k20 = deg2rad(yss20)/(15-13.03); %amplitude do degrau de entrada eh (15-13.03)
+
+alfa20  = omega_n20^2/cos(deg2rad(20))
+beta20  = 2*ksi20*omega_n20
+gamma20 = k20 * omega_n20^2
---
+++ lab2/lab3_calculos_40.m
@@ -0,0 +1,21 @@
+clear all;
+
+load("40_percent.mat")
+plot(out.simout_30.Time, out.simout_30.Data(1:end,2));
+
+tp40 = 20.75 - 20.117;
+yp40 = 52.245 - 43.155;
+yss40 = 48.69 - 43.155;
+
+Mp40 = (yp40-yss40)/yss40;
+
+ksi40 = -log(Mp40)/sqrt(pi^2 + (log(Mp40))^2);
+omega_n40 = pi/(tp40*sqrt(1-ksi40^2));
+k40 = deg2rad(yss40)/(30-25.16); %amplitude do degrau a ser preenchida.
+% 
+% coloca o final menos o que estava antes, verificar o arquivo _20
+%
+
+alfa40  = omega_n40^2/cos(deg2rad(40))
+beta40  = 2*ksi40*omega_n40
+gamma40 = k40 * omega_n40^2
