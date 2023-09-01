ff43782a80915efda3eb12b834f8b7cf6bdfe5b2
Paulo Vitor M.Andrade <100442352+itapops@users.noreply.github.com>
2023-08-31 22:15:58 -0300

    Correção no codigo da analise a 20 graus

--- lab2/lab3_calculos_20.m
+++ lab2/lab3_calculos_20.m
@@ -10,7 +10,7 @@ Mp20 = (yp20-yss20)/yss20;
 
 ksi20 = -log(Mp20)/sqrt(pi^2 + (log(Mp20))^2);
 omega_n20 = pi/(tp20*sqrt(1-ksi20^2));
-k20 = deg2rad(yss20)/(15-13.03); %amplitude do degrau de entrada eh (15-13.03)
+k20 = deg2rad(yss20/(15-13.03)); %amplitude do degrau de entrada eh (15-13.03)
 
 alfa20  = omega_n20^2/cos(deg2rad(20))
 beta20  = 2*ksi20*omega_n20
